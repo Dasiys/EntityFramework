faa3ee0e56f0cebd6c064bba12aa5b25dd6266fe
Dasiys <yeweimi@iCloud.com>
2017-05-09 17:41:15 +0800

    添加视图

--- EfDemo/EfDemo/Controllers/StudentController.cs
+++ EfDemo/EfDemo/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Model;
+using Infastrcuture;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,24 @@ namespace EfDemo.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly StudentService _stidentService=new StudentService(new StudentDbContext());
         // GET: Student
         public ActionResult Index()
         {
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student entity)
+        {
+            if(ModelState.IsValid)
+                _stidentService.AddStudent(entity);
             return View();
         }
     }
