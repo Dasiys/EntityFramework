4312361b36601a74bc7c5251f6e3a106600e23c1
Dasiys <yeweimi@iCloud.com>
2017-05-11 15:55:35 +0800

    console的Demo

---
+++ EfConsoleDemo/EfConsoleDemo/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfConsoleDemo
+{
+    public class UserContext:DbContext
+    {
+        public UserContext() : base("UserDb")
+        {
+            
+        }
+
+        public DbSet<User> User { set; get; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.Add(new UserMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
