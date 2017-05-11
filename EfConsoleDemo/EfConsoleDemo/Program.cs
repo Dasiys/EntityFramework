383ac3ac16d5ed040cbdf5e6f6ceb2b7708dcf1e
Dasiys <yeweimi@iCloud.com>
2017-05-11 16:07:10 +0800

    测试TransactionScope

--- EfConsoleDemo/EfConsoleDemo/Program.cs
+++ EfConsoleDemo/EfConsoleDemo/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.Win32;
 
 namespace EfConsoleDemo
 {
@@ -10,6 +13,42 @@ namespace EfConsoleDemo
     {
         static void Main(string[] args)
         {
+            try
+            {
+                using (var scope = new TransactionScope())
+                {
+                    using (var db = new UserContext())
+                    {
+                        var user = db.User;
+                        user.Add(new User()
+                        {
+                            Name = "叶伟密"
+                        });
+                        db.SaveChanges();
+                        var userCount = db.User.Count();
+                        Console.WriteLine("用户数目:{0}",userCount);
+                    }
+
+                    using (var db = new UserContext())
+                    {
+                        db.User.Add(new User()
+                        {
+                            Name = "小花999花花花花"
+                        });
+                        db.SaveChanges();
+                    }
+                    scope.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            using (var db=new UserContext())
+            {
+                Console.WriteLine("最终用户数目:{0}",db.User.Count());
+            }
+            Console.ReadLine();
         }
     }
 }
